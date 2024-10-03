2581bd01dae593d79e89a752ec698e534dc70efc
blastonyz <blastonyzamora@gmail.com>
2024-10-03 14:07:21 -0300

    enviroment-scale

--- app/components/dimensions3/Render.jsx
+++ app/components/dimensions3/Render.jsx
@@ -9,6 +9,7 @@ import * as THREE from 'three';
 
 const Model = ({ url }) => {
   const { scene } = useGLTF(url);
+
   return <primitive object={scene} />;
 };
 
@@ -20,8 +21,6 @@ const Piso = () => {
   );
 };
 
-
-
   const HDRIBackground = () => {
     const { scene, camera } = useThree(); // Obtener la escena del Canvas
   
@@ -31,27 +30,35 @@ const Piso = () => {
         texture.mapping = EquirectangularReflectionMapping;
         scene.environment = texture; // Asignar textura de entorno
         scene.background = texture;    // Asignar textura de fondo
+        texture.repeat.set(0.5, 0.5); // Ajusta la repetición de la textura
+        texture.wrapS = THREE.RepeatWrapping; // Repite la textura en el eje S
+        texture.wrapT = THREE.RepeatWrapping; // Repite la textura en el eje T
         if (scene) {
           const bbox = new THREE.Box3().setFromObject(scene);
           const center = bbox.getCenter(new THREE.Vector3());
           const size = bbox.getSize(new THREE.Vector3()).length();
       
           // Ajustar la posición de la cámara según el tamaño del modelo
-          camera.position.set(center.x, center.y + size / 2, center.z + size * 2);
+          camera.position.set(center.x, center.y + size / 4, center.z + size); // Acércate al modelo
           camera.lookAt(center);
+    
+          // Cambiar el FOV de la cámara si es necesario
+          camera.fov = 30; // Ajustar el campo de visión
+          camera.updateProjectionMatrix();
         }
       
       }, undefined, (error) => {
         console.error('Error loading HDRI:', error); // Manejar errores de carga
       });
-    }, [scene]);
+    }, [scene,camera]);
   
     return null; // No se necesita renderizar nada
   };
 
+
 const Render = () => {
   return (
-    <Canvas camera={{ position: [-4, 2, 7] }}>
+    <Canvas camera={{ position: [-4, 2, 7] , rotation: [-Math.PI / 2, -3, 1]} } className='mainScene' >
       <ambientLight intensity={1} />
       <directionalLight position={[0, 0, 5]} />
       <directionalLight color="red" position={[0, 1, 5]} />
@@ -65,13 +72,13 @@ const Render = () => {
       </Suspense>
 
       <Piso />
-      <OrbitControls  
+      
+      <OrbitControls 
           enableZoom={true} 
           enablePan={true} 
           maxPolarAngle={Math.PI / 2} 
+          minPolarAngle={0} // Puedes ajustar estos valores
       />
-
-      {/* Cargar el fondo HDRI */}
       <HDRIBackground />
     </Canvas>
   );
